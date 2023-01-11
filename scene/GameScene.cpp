78d79860f09e7a36c97f5e47e9f14d015d2fb7a7
takei_yuushin0831 <k021g1262@g.neec.ac.jp>
2023-01-11 13:18:51 +0900

    プロジェクト ファイルを追加します。

---
+++ scene/GameScene.cpp
@@ -0,0 +1,94 @@
+﻿#include "GameScene.h"
+#include "TextureManager.h"
+#include <cassert>
+
+//コンストラクタ
+GameScene::GameScene() {}
+
+//デストラクタ
+GameScene::~GameScene() 
+{
+	delete spriteBG_;//BG
+	delete modelStage_;//ステージ
+}
+
+//初期化
+void GameScene::Initialize() 
+{
+
+	dxCommon_ = DirectXCommon::GetInstance();
+	input_ = Input::GetInstance();
+	audio_ = Audio::GetInstance();
+	debugText_ = DebugText::GetInstance();
+
+	//BG(スプライト)
+	textureHandlBG_ = TextureManager::Load("bg.jpg");
+	spriteBG_ = Sprite::Create(textureHandlBG_, { 0,0 });
+
+	//ビュープロジェクションの初期化
+	viewProjection_.eye = { 0,1,-6 };
+	viewProjection_.target = { 0,1,0 };
+	viewProjection_.Initialize();
+
+	//ステージ
+	textureHandlStage_ = TextureManager::Load("stage.jpg");
+	modelStage_ = Model::Create();
+	worldTransformStage_.translation_ = { 0,-1.5f,0 };
+	worldTransformStage_.scale_ = { 4.5f,1,40 };
+	worldTransformStage_.Initialize();
+}
+
+//更新
+void GameScene::Update() {}
+
+//表示
+void GameScene::Draw() {
+
+	// コマンドリストの取得
+	ID3D12GraphicsCommandList* commandList = dxCommon_->GetCommandList();
+
+#pragma region 背景スプライト描画
+	// 背景スプライト描画前処理
+	Sprite::PreDraw(commandList);
+
+	/// <summary>
+	/// ここに背景スプライトの描画処理を追加できる
+	/// </summary>
+	spriteBG_->Draw();
+
+	// スプライト描画後処理
+	Sprite::PostDraw();
+	// 深度バッファクリア
+	dxCommon_->ClearDepthBuffer();
+#pragma endregion
+	
+#pragma region 3Dオブジェクト描画
+	// 3Dオブジェクト描画前処理
+	Model::PreDraw(commandList);
+
+	/// <summary>
+	/// ここに3Dオブジェクトの描画処理を追加できる
+	/// </summary>
+	//ステージ
+	modelStage_->Draw(worldTransformStage_, viewProjection_, textureHandlStage_);
+
+	// 3Dオブジェクト描画後処理
+	Model::PostDraw();
+#pragma endregion
+
+#pragma region 前景スプライト描画
+	// 前景スプライト描画前処理
+	Sprite::PreDraw(commandList);
+
+	/// <summary>
+	/// ここに前景スプライトの描画処理を追加できる
+	/// </summary>
+
+	// デバッグテキストの描画
+	debugText_->DrawAll(commandList);
+	//
+	// スプライト描画後処理
+	Sprite::PostDraw();
+
+#pragma endregion
+}
---
+++ scene/GameScene.h
@@ -0,0 +1,64 @@
+﻿#pragma once
+
+#include "Audio.h"
+#include "DirectXCommon.h"
+#include "DebugText.h"
+#include "Input.h"
+#include "Model.h"
+#include "SafeDelete.h"
+#include "Sprite.h"
+#include "ViewProjection.h"
+#include "WorldTransform.h"
+
+/// <summary>
+/// ゲームシーン
+/// </summary>
+class GameScene {
+
+  public: // メンバ関数
+	/// <summary>
+	/// コンストクラタ
+	/// </summary>
+	GameScene();
+
+	/// <summary>
+	/// デストラクタ
+	/// </summary>
+	~GameScene();
+
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	void Initialize();
+
+	/// <summary>
+	/// 毎フレーム処理
+	/// </summary>
+	void Update();
+
+	/// <summary>
+	/// 描画
+	/// </summary>
+	void Draw();
+
+  private: // メンバ変数
+	DirectXCommon* dxCommon_ = nullptr;
+	Input* input_ = nullptr;
+	Audio* audio_ = nullptr;
+	DebugText* debugText_ = nullptr;
+
+	/// <summary>
+	/// ゲームシーン用
+	/// </summary>
+	
+	//BG(スプライト)
+	uint32_t textureHandlBG_ = 0;
+	Sprite* spriteBG_ = nullptr;
+	//ビュープロジェクション(共通)
+	ViewProjection viewProjection_;
+	//ステージ
+	uint32_t textureHandlStage_ = 0;
+	Model* modelStage_ = nullptr;
+	WorldTransform worldTransformStage_;
+};
+
